de0b66756e1f18866ac9ced40f88d2186ac48d1e
Jan Aagaard <jaa@webcrm.com>
2018-03-22 14:59:37 +0100

    Add GetCustomers method

---
+++ PowerofficeConnector/Models/Customer.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Webcrm.Integrations.PowerofficeConnector.Models
+{
+    public class Customer
+    {
+        public DateTime since { get; set; }
+        public int id { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string name { get; set; }
+    }
+}
--- PowerofficeConnector/PowerofficeClient.cs
+++ PowerofficeConnector/PowerofficeClient.cs
@@ -1,4 +1,7 @@
+using RestSharp;
 using System;
+using System.Collections.Generic;
+using Webcrm.Integrations.PowerofficeConnector.Models;
 
 namespace Webcrm.Integrations.PowerofficeConnector
 {
@@ -12,7 +15,41 @@ namespace Webcrm.Integrations.PowerofficeConnector
             ClientKey = clientKey;
         }
 
+        private const string baseApiUrl = "https://api-demo.poweroffice.net/";
+        private const string baseAuthenticationUrl = "https://godemo.poweroffice.net/";
+
         private string ApplicationKey { get; }
         private string ClientKey { get; }
+
+        public List<Customer> GetCustomers()
+        {
+            var client = new RestClient(baseAuthenticationUrl);
+
+            string encodedKeys = Base64Encode($"{ApplicationKey}:{ClientKey}");
+
+            var tokensRequest = new RestRequest("OAuth/Token", Method.POST);
+            tokensRequest.AddHeader("Authorization", encodedKeys);
+            tokensRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+            tokensRequest.AddBody("grant_type=client_credentials");
+
+            var tokensResponse = client.Execute<AuthorizationTokens>(tokensRequest);
+
+            client.BaseUrl = new System.Uri(baseApiUrl);
+            var customersRequest = new RestRequest("Customer/", Method.GET);
+            customersRequest.AddHeader("Authorization", tokensResponse.Data.access_token);
+
+            var rawCustomersResponse = client.Execute(customersRequest);
+
+            var cutomersResponse = client.Execute<List<Customer>>(customersRequest);
+
+            return cutomersResponse.Data;
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            string encodedText = System.Convert.ToBase64String(plainTextBytes);
+            return encodedText;
+        }
     }
 }
