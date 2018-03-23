4bd04be86aa5d5a1a48992341bfb7115e13a8751
Jan Aagaard <jaa@webcrm.com>
2018-03-23 11:20:20 +0100

    Sort alphabetically

--- Private/ApiKeys.sample.cs
+++ Private/ApiKeys.sample.cs
@@ -2,13 +2,13 @@
 //{
 //    public static class ApiKeys
 //    {
-//        public const string WebcrmB2bFullAccessAppToken = "";
-//        public const string WebcrmB2bReadOnlyAccessAppToken = "";
-
 //        public const string FortnoxAccessToken = "";
 //        public const string FortnoxClientSecret = "";
-
+//
 //        public const string PowerOfficeTestApplicationKey = "";
 //        public const string PowerOfficeTestClientKey = "";
+//
+//        public const string WebcrmB2bFullAccessAppToken = "";
+//        public const string WebcrmB2bReadOnlyAccessAppToken = "";
 //    }
 //}
